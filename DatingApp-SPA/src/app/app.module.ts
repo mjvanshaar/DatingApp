6d7408b079d11887e3d6cefbaae7c035c6c02734
Michael VanShaar <michaelvanshaar@Michaels-MacBook-Pro.local>
2020-06-11 14:39:18 -0600

    End of Section 10

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -26,6 +26,9 @@ import { AlertifyService } from './_services/alertify.service';
 import { UserService } from './_services/user.service';
 import { AuthGuard } from './_guards/auth.guard';
 import { MemberListResolver } from './_resolvers/member-list.resolver';
+import { MemberEditComponent } from './members/member-edit/member-edit.component';
+import { MemberEditResolver } from './_resolvers/member-edit.resolver';
+import { PreventUnsavedChanges } from './_guards/prevent-unsaved-changes.guard';
 
 export function tokenGetter() {
    return localStorage.getItem('token');
@@ -48,7 +51,8 @@ export class CustomHammerConfig extends HammerGestureConfig {
       ListsComponent,
       MessagesComponent,
       MemberCardComponent,
-      MemberDetailComponent
+      MemberDetailComponent,
+      MemberEditComponent
    ],
    imports: [
       BrowserModule,
@@ -68,10 +72,17 @@ export class CustomHammerConfig extends HammerGestureConfig {
       })
    ],
    providers: [
+      AuthService,
       ErrorInterceptorProvider,
+      AlertifyService,
+      AuthGuard,
       MemberDetailResolver,
       MemberListResolver,
-      {provide: HAMMER_GESTURE_CONFIG, useClass: CustomHammerConfig}
+      {provide: HAMMER_GESTURE_CONFIG, useClass: CustomHammerConfig},
+      MemberEditResolver,
+      UserService,
+      PreventUnsavedChanges ,
+
    ],
    bootstrap: [
       AppComponent
